802469e650c78f9164fd35d2b8fdd2782a01ae09
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-05-27 14:01:59 +0200

    uninstall next-export-image

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate');
 
-const withPlugins = require('next-compose-plugins');
-const withExportImages = require('next-export-optimize-images');
-
-// module.exports = {
-//   ...nextTranslate(),
-// };
-
-module.exports = withPlugins(
-  [
-    withExportImages,
-    // your other plugins here
-  ],
-  {
-    ...nextTranslate(),
-  }
-);
+module.exports = {
+  ...nextTranslate(),
+};
