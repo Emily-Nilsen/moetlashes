202770cc96294f9e1e1111c13949d91bd1c01b1f
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-05-27 13:56:02 +0200

    Add next-export-optimize-images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate');
 
-module.exports = {
-  ...nextTranslate(),
-};
+const withPlugins = require('next-compose-plugins');
+const withExportImages = require('next-export-optimize-images');
+
+// module.exports = {
+//   ...nextTranslate(),
+// };
+
+module.exports = withPlugins(
+  [
+    withExportImages,
+    // your other plugins here
+  ],
+  {
+    ...nextTranslate(),
+  }
+);
