db7915e3684207ac1a6e9c632ea44288a0887136
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2022-05-27 19:43:13 +0200

    Revert to cloudinary domain in config file

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate');
 
-const withExportImages = require('next-export-optimize-images');
+// const withExportImages = require('next-export-optimize-images');
 
-module.exports = withExportImages({
+module.exports = {
   ...nextTranslate(),
   images: {
     domains: ['res.cloudinary.com'],
   },
-});
+};
